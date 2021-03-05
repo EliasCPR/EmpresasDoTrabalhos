d0159cd3e01211450f5c8ee087c70b5cb07c587b
Elias Oliveira <eliascpr123@gmail.com>
2021-03-05 16:35:23 -0300

    Update calculaEuroDolar.php

--- XDInvestimentos/calculaEuroDolar.php
+++ XDInvestimentos/calculaEuroDolar.php
@@ -3,8 +3,8 @@
     $reais = $_GET["valorReais"];
     $unidadeMonetaria = $_GET["unidadeMonetaria"];
 
-    $convercaoDolar = $reais * 5.41;
-    $convercaoEuro = $reais * 6.57;
+    $convercaoDolar = $reais / 5.41;
+    $convercaoEuro = $reais / 6.57;
 
     if ($unidadeMonetaria == "Dolar"){
         echo "<h1>Valor total em $unidadeMonetaria: $ $convercaoDolar</h1>";
